54d36675f9ce53545dc913482cd8a7e107b577db
digitaldonkey <tho@donkeymedia.eu>
2023-10-03 14:40:21 +0200

    Fix test runner (not the tests :/ ).

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
 })
