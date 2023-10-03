1ba0fb3edbd9aa0df07b2ef3846538d9522831f8
digitaldonkey <tho@donkeymedia.eu>
2023-10-03 15:47:19 +0200

    Add some codesplitting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    splitVendorChunkPlugin()
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // creating a chunk to @open-ish deps. Reducing the vendor chunk size
+          // if (id.includes('react-tooltip')) {
+          //   return 'react-tooltip';
+          // }
+          // creating a chunk to react routes deps. Reducing the vendor chunk size
+          if (
+            id.includes('react-tooltip') ||
+            id.includes('react-select') ||
+            id.includes('recharts')
+          ) {
+            return 'ui-components';
+          }
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'happy-dom'
